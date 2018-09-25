46bb2296ac12e1e2fd59991f777c22d6e72d0ffc
Donna Cadiente <djanecad@hotmail.com>
2018-09-25 06:50:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocks){
+  if (blocks >42){
+  return blocks -42 
+  }
+  else if (blocks <42){
+    return 42 - blocks
+  }
+}
+
+function distanceFromHqinFeet (hqfeet){
+  distanceFromHqInBlocks()
+}
+
+function distanceTravelledInFeet (feet){
+  
+}
